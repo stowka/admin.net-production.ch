424a663c663c9efe766b4dfc3302a6a86a89f874
Antoine De Gieter <antoine.degieter@net-production.ch>
2015-03-17 11:34:48 +0100

    GUI v2

---
+++ www/views/commitments.view.php
@@ -0,0 +1,32 @@
+<?php
+	/**
+	 * Default home view
+	 * @author Antoine De Gieter
+	 */
+?>
+
+<!doctype html>
+<html lang="en_UK">
+	<?php includeSection('head') ?>
+
+	<body>
+		<div id="wrapper">
+			<?php includeSection('sidebar'); ?>
+
+			<div id="page-content-wrapper">
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+							<h1>
+								Commitments
+							</h1>
+							<p>
+								Start here...
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
---
+++ www/views/sections/sidebar.sec.php
@@ -0,0 +1,15 @@
+ <!-- Sidebar -->
+<div id="sidebar-wrapper">
+	<ul class="sidebar-nav">
+		<li class="sidebar-brand">
+			<img src="global/img/logos/logo-sm.png" alt="Logo Net Production" width="64px">
+		</li>
+		<li><a href="./?page=stats">Stats</a></li>
+		<li class="active"><a href="./?page=home">Home</a></li>
+		<li><a href="./?page=projects">Projects</a></li>
+		<li><a href="./?page=commitments">Commitments</a></li>
+		<li><a href="./?page=team">Team</a></li>
+		<li><a href="./?page=contact">Contact</a></li>
+	</ul>
+</div>
+<!-- /#sidebar-wrapper -->
