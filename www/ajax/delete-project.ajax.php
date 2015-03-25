d059f4b1bebf80c82b8820d7a3918796cc1362cb
MrPwet <beligat.fx@gmail.com>
2015-03-25 12:42:13 +0100

    The picture of a project is now deleted when this project is deleted

--- www/ajax/delete-project.ajax.php
+++ www/ajax/delete-project.ajax.php
@@ -5,7 +5,14 @@
 
     if(isset($_POST['id'])) {
         $project = Project::initWithId($_POST['id']);
+
+        $target_dir = "../global/img/uploads/projects/";
+        $name = $_POST['id'] . ".png";
+
+        unlink($target_dir . $name);
+
         $project->delete();
+
         echo $_POST['id'];
     }
 
